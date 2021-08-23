f4a5a759d0b7f11d24d559b945c6ac388d7ee457
Gustavo Gonçalves <gustavoprof3@gmail.com>
2021-08-23 19:17:26 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(path.resolve(), '/client/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(path.resolve(), 'client', 'build', 'index.html'))
   );
 } else {
   app.get('/api', async (req, res) => {
