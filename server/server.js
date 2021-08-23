58260ea79f7fd3703ccf44eac60b3947029f41cf
Gustavo Gonçalves <gustavoprof3@gmail.com>
2021-08-23 19:13:46 -0300

    deploy second attempt

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(json());
 app.use(authentication);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(path.resolve(), '/frontend/build')));
+  app.use(express.static(path.join(path.resolve(), '/client/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 } else {
   app.get('/api', async (req, res) => {
