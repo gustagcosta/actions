16183e92fe9d3c317750a3178e1e8e709c14f774
Gustavo Gonçalves <gustavoprof3@gmail.com>
2021-08-29 03:45:08 -0300

    testing github.dev

--- server/api/tasks.js
+++ server/api/tasks.js
@@ -7,7 +7,9 @@ const router = express.Router();
 // @desc      Send all tasks to logged  user
 // @route     GET /api/tasks
 // @access    Common
-router.get('/todo', authorization('common', 'admin'), async (req, res) => {});
+router.get('/todo', authorization('common', 'admin'), async (req, res) => {
+  res.json(req.db.select('*').from('tasks').where('id_target', '=', req.user.id))
+});
 
 // @desc      Send all tasks to manager
 // @route     GET /api/tasks
@@ -15,13 +17,17 @@ router.get('/todo', authorization('common', 'admin'), async (req, res) => {});
 router.get(
   '/assigned',
   authorization('manager', 'admin'),
-  async (req, res) => {}
+  async (req, res) => {
+    res.json(req.db.select('*').from('tasks').where('id_manager', '=', req.user.id))  
+  }
 );
 
 // @desc      Send task details
 // @route     POST /api/tasks/
 // @access    Common
-router.get('/:id', authorization('common', 'admin'), async (req, res) => {});
+router.get('/:id', authorization('common', 'admin'), async (req, res) => {
+  res.json(req.db.select('*').from('tasks').where('id', '=', req.params.id))
+});
 
 // @desc      Store new task
 // @route     POST /api/tasks/
@@ -31,7 +37,9 @@ router.post('/', authorization('manager', 'admin'), async (req, res) => {});
 // @desc      Update a task
 // @route     PUT /api/tasks/
 // @access    Manager
-router.put('/:id', authorization('admin', 'manager'), async (req, res) => {});
+router.put('/:id', authorization('admin', 'manager'), async (req, res) => {
+
+});
 
 // @desc      Delete a task
 // @route     DELETE /api/tasks/
@@ -39,8 +47,9 @@ router.put('/:id', authorization('admin', 'manager'), async (req, res) => {});
 router.delete(
   '/:id',
   authorization('admin', 'manager'),
-  async (req, res) => {}
-);
+  async (req, res) => {
+
+});
 
 // @desc      Conclude a task
 // @route     PATCH /api/tasks/:id
